90f1683cbf5e47edc71c2c62180cd6a0718d9724
zhongvera <vzhong@andrew.cmu.edu>
2024-06-10 23:37:10 -0400

    fixed task name not showing up bug

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,20 +14,32 @@ class Home extends Component {
   }
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
   // updates the state with the new list.
+  // addTodo = (todo) => {
+  //   // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
+  //   // should not share duplicate values.
+  //   // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
+  //   // This solution works for a small application but a more complex hashing function should be used when
+  //   // dealing with a larger data sensitive project.
+  //   todo.id = Math.random();
+  //   // An array that contains the current array and the new todo item
+  //   let new_list = [...this.state.todos, todo];
+  //   // Updates the local state with the new array.
+  //   this.setState({
+  //     todos: new_list,
+  //   });
+  // };
+
   addTodo = (todo) => {
-    // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
-    // should not share duplicate values.
-    // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
-    // This solution works for a small application but a more complex hashing function should be used when
-    // dealing with a larger data sensitive project.
-    todo.id = Math.random();
-    // An array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Updates the local state with the new array.
+    const newTodo = {
+        id: Math.random(),
+        text: todo.content  // Use the content from the passed todo object
+    };
+    let new_list = [...this.state.todos, newTodo];
     this.setState({
-      todos: new_list,
+        todos: new_list,
     });
   };
+
   render() {
     return (
       <div className="Home">
